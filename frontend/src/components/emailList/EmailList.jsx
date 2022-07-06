a8673c013d6e16f7dcbca99c6a08900eefe66243
advait-cs <1962advaitad@gmail.com>
2022-07-06 16:07:20 +0530

    update

---
+++ frontend/src/components/emailList/EmailList.jsx
@@ -0,0 +1,16 @@
+import "./emailList.css"
+
+const EmailList = () => {
+  return (
+    <div className="mail">
+    <h1 className="mailTitle">Save time, save money!</h1>
+    <span className="mailDesc">Sign up and we'll send the best deals to you</span>
+    <div className="mailContainer">
+      <input type="text" placeholder="Your email"/>
+      <button>Subscribe</button>
+    </div>
+    </div>
+  )
+}
+
+export default EmailList;
